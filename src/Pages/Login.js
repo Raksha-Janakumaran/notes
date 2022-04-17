4471d7193b5749a6d7fae19556c29eb93b3df36c
Joshua Joseph <josephjoshua341@gmail.com>
2022-04-17 18:38:48 +0530

    Login Page UI completed

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,107 +1,37 @@
-import { useState } from 'react';
+import React from 'react'
 
-export default function Login() {
-
-
-const [firstname, setFirstname] = useState('');
-const [lastname, setLastname] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-
-
-const [submitted, setSubmitted] = useState(false);
-const [error, setError] = useState(false);
-
-
-const handleFirstname = (e) => {
-	setFirstname(e.target.value);
-	setSubmitted(false);
-};
-
-const handleLastname =(e) => {
-    setLastname(e.target.value);
-    setSubmitted(false);
+function Login() {
+  return (
+	<div className="container-lg">
+        <div className="row" style={{minHeight: '100vh'}}>
+            <div className="col-6 d-flex align-items-center justify-content-center">
+                <img class="img-fluid" src={require('../Assets/Signin.jpg')}/>
+			</div>
+			<div className="col-6 d-flex align-items-center justify-content-center">
+			  <div style={{width:'100%'}}>
+				 <center> 
+				 <div className="h1 mb-3">Login</div> 	
+                 <div style={{width:'80%'}}>
+					<div className="mb-3">
+						<div className="col-sm-10">
+						   <input type="text" class="form-control" id="Email" placeholder='Email'/>
+						</div>
+					</div>
+				 </div>
+				 <div style={{width:'80%'}}>
+					<div className="mb-3">
+						<div className="col-sm-10">
+						   <input type="password" class="form-control" id="inputPassword" placeholder='Password'/>
+						</div>
+					</div>
+				 </div>
+				 <button className="btn btn-primary">Submit</button>
+				 </center>
+		      </div>		 
+			</div>
+		</div>
+	</div>
+  )
 }
 
-
-const handleEmail = (e) => {
-	setEmail(e.target.value);
-	setSubmitted(false);
-};
-
-
-const handlePassword = (e) => {
-	setPassword(e.target.value);
-	setSubmitted(false);
-};
-
-
-const handleSubmit = (e) => {
-	e.preventDefault();
-	if (firstname === '' || lastname === '' || email === '' || password === '') {
-	setError(true);
-	} else {
-	setSubmitted(true);
-	setError(false);
-	}
-};
-
-
-const successMessage = () => {
-	return (
-	<div
-		className="success"
-		style={{
-		display: submitted ? '' : 'none',
-		}}>
-		<h1>User {firstname} {lastname} successfully registered!!</h1>
-	</div>
-	);
-};
-
-
-const errorMessage = () => {
-	return (
-	<div
-		className="error"
-		style={{
-		display: error ? '' : 'none',
-		}}>
-		<h1>Please enter all the fields</h1>
-	</div>
-	);
-};
-
-return (
-    <div className="container">
-	<div className="form">
-	<div>
-		<h1>User Registration</h1>
-	</div>
-
-	
-	<div className="messages">
-		{errorMessage()}
-		{successMessage()}
-	</div>
-    
-	<form>
-		
-		<label className="label">First Name</label>
-		<input onChange={handleFirstname} className="input" value={firstname} type="text" />
-
-        <label className="label">Last Name</label>
-		<input onChange={handleLastname} className="input" value={lastname} type="text" />
-
-		<label className="label">Email</label>
-		<input onChange={handleEmail} className="input" value={email} type="email" />
-
-		<label className="label">Password</label>
-		<input onChange={handlePassword} className="input" value={password} type="password" />
-
-		<button onClick={handleSubmit} className="btn" type="submit"> Submit </button>
-	</form>
-    </div>
-	</div>
-);
-}
+export default Login
